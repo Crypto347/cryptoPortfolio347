803c36576003fcfaa9d9d8e7d497a46e64e394d1
Humay <crypto347@gmail.com>
2020-07-29 17:36:43 +0400

    menuItems array updated

--- src/constants/menuItems.js
+++ src/constants/menuItems.js
@@ -5,7 +5,200 @@ export const menuItemsArray = [
         itemId: "home",
         path: "",
         active: true,
-        options: []
+        options: [
+            {
+                id: 11,
+                header: null,
+                itemId: null,
+                array: [
+                    {
+                        id: 111,
+                        text: "Main Home",
+                        itemId: "mainHome",
+                        path: "",
+                        active: true,
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 112,
+                        text: "Custom Project 2",
+                        itemId: "customProject2",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 113,
+                        text: "Big images",
+                        itemId: "bigImages",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 114,
+                        text: "Big slider",
+                        itemId: "bigSlider",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 115,
+                        text: "Small images",
+                        itemId: "smallImages",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 115,
+                        text: "Small images",
+                        itemId: "smallImages",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                ]
+            },
+            {
+                id: 12,
+                header: null,
+                itemId: null,
+                array: [
+                    {
+                        id: 121,
+                        text: "Photography Home",
+                        itemId: "photographyHome",
+                        path: "",
+                        active: true,
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 122,
+                        text: "Shop Divided",
+                        itemId: "shopDivided",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 123,
+                        text: "Project Showcase",
+                        itemId: "projectShowcase",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 124,
+                        text: "Boxed Showcase",
+                        itemId: "boxedShowcase",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 125,
+                        text: "Portfolio Gallery",
+                        itemId: "portfolioGallery",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    }
+                ]
+            },
+            {
+                id: 13,
+                header: null,
+                itemId: null,
+                array: [
+                    {
+                        id: 131,
+                        text: "Conference Home",
+                        itemId: "conferenceHome",
+                        path: "",
+                        active: true,
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 132,
+                        text: "Shop Home",
+                        itemId: "shopHome",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 133,
+                        text: "Masonry Portfolio",
+                        itemId: "masonryPortfolio",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 134,
+                        text: "Split Screen Slider",
+                        itemId: "splitScreenSlider",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 135,
+                        text: "Fullscreen Showcase",
+                        itemId: "fullscreenShowcase",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    }
+                ]
+            },
+            {
+                id: 14,
+                header: null,
+                itemId: null,
+                array: [
+                    {
+                        id: 141,
+                        text: "Marketing Agency",
+                        itemId: "marketingAgency",
+                        path: "",
+                        active: true,
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 142,
+                        text: "Shop Masonry",
+                        itemId: "shopMasonry",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 143,
+                        text: "Parallax Portfolio",
+                        itemId: "parallaxPortfolio",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 144,
+                        text: "vCard Home",
+                        itemId: "vCardHome",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    }
+                ]
+            }
+        ]
     },
     {
         id: 2,
@@ -13,7 +206,256 @@ export const menuItemsArray = [
         itemId: "portfolio",
         path: "portfolio",
         active: false,
-        options: []
+        options: [
+            {
+                id: 21,
+                header: "Featured Projects",
+                itemId: "featuredProjects",
+                array: [
+                    {
+                        id: 211,
+                        text: "Custom Project 1",
+                        itemId: "customProject1",
+                        path: "",
+                        active: true,
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 212,
+                        text: "Custom Project 2",
+                        itemId: "customProject2",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 213,
+                        text: "Big images",
+                        itemId: "bigImages",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 214,
+                        text: "Big slider",
+                        itemId: "bigSlider",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 215,
+                        text: "Small images",
+                        itemId: "smallImages",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 215,
+                        text: "Small slider",
+                        itemId: "smallSlider",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 116,
+                        text: "Gallery",
+                        itemId: "gallery",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 217,
+                        text: "Small galery",
+                        itemId: "smallGallery",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                ]
+            },
+            {
+                id: 22,
+                header: "Columns",
+                itemId: "columns",
+                array: [
+                    {
+                        id: 221,
+                        text: "Two Columns",
+                        itemId: "twoColumns",
+                        path: "",
+                        active: true,
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 222,
+                        text: "Two Columns Wide",
+                        itemId: "twoColumnsWide",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 223,
+                        text: "Three Columns",
+                        itemId: "threeColumns",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 224,
+                        text: "Three Columns Wide",
+                        itemId: "threeColumnsWide",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 225,
+                        text: "Four Columns",
+                        itemId: "fourColumns",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 226,
+                        text: "Four Columns Wide",
+                        itemId: "fourColumnsWide",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 227,
+                        text: "Five Columns Wide",
+                        itemId: "fiveColumnsWide",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    }
+                ]
+            },
+            {
+                id: 23,
+                header: "Hover types",
+                itemId: "hoverTypes",
+                array: [
+                    {
+                        id: 231,
+                        text: "Overlay",
+                        itemId: "overlay",
+                        path: "",
+                        active: true,
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 232,
+                        text: "Overlay With Info",
+                        itemId: "overlayWithInfo",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 233,
+                        text: "Simple Overlay",
+                        itemId: "simpleOverlay",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 234,
+                        text: "Slide From Image Left",
+                        itemId: "slideFromImageLeft",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 235,
+                        text: "Switch Image",
+                        itemId: "switchImage",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    }
+                ]
+            },
+            {
+                id: 24,
+                header: "Portfolio Types",
+                itemId: "portfolioTypes",
+                array: [
+                    {
+                        id: 241,
+                        text: "Standard",
+                        itemId: "standard",
+                        path: "",
+                        active: true,
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 242,
+                        text: "Gallery",
+                        itemId: "gallery",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 243,
+                        text: "Gallery With Space",
+                        itemId: "galleryWithSpace",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 244,
+                        text: "Masonry",
+                        itemId: "masonry",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 245,
+                        text: "Masonry Wide",
+                        itemId: "masonryWide",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 246,
+                        text: "Metro",
+                        itemId: "metro",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    },
+                    {
+                        id: 247,
+                        text: "Pinterest 3 columns",
+                        itemId: "pinterest3Columns",
+                        path: "",
+                        active: false,
+                        subOptions: []
+                    }
+                ]
+            }
+        ]
     },
     {
         id: 3,
