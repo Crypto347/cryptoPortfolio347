f0db472ba94f149f0c7e0a9dd2c37b90a350087b
Humay <crypto347@gmail.com>
2020-08-11 14:30:09 +0400

    code cleaning 15

--- src/components/UtilityComponents/index.js
+++ src/components/UtilityComponents/index.js
@@ -10,9 +10,6 @@ export * from './FontSize/H65/h65';
 export * from './FontSize/H70/h70';
 export * from './FontSize/H130/h130';
 
-
-
-
 export * from './EmptyDivs/EH10/eh10';
 export * from './EmptyDivs/EH20/eh20';
 export * from './EmptyDivs/EH25/eh25';
