93af69b90f12486b93f006d09b7fd4354056946a
Humay <qasimovahumay@gmail.com>
2020-08-26 14:24:26 +0400

    menuFullScreenItems array in costants added

---
+++ src/constants/menuFullscreenItems.js
@@ -0,0 +1,168 @@
+export const menuItemsArray = [
+    {
+        id: 1,
+        text: "Home.",
+        itemId: "home",
+        path: null,
+        active: true,
+        isHover: "init",
+        hasSubOptions: false,
+        options: [
+            {
+                id: 11,
+                header: null,
+                itemId: null,
+                array: [
+                    {
+                        id: 111,
+                        text: "Design Studio",
+                        itemId: "designStudio",
+                        path: "",
+                        active: true,
+                        isHover: "init",
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 112,
+                        text: "Marketing Agency",
+                        itemId: "marketingAgency",
+                        path: "",
+                        active: false,
+                        isHover: "init",
+                        subOptions: []
+                    },
+                    {
+                        id: 113,
+                        text: "Conference Home",
+                        itemId: "conferenceHome",
+                        path: "",
+                        active: false,
+                        isHover: "init",
+                        subOptions: []
+                    },
+                    {
+                        id: 114,
+                        text: "Main Home",
+                        itemId: "mainHome",
+                        path: "",
+                        active: false,
+                        isHover: "init",
+                        subOptions: []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: "About.",
+        itemId: "about",
+        path: null,
+        active: true,
+        isHover: "init",
+        hasSubOptions: false,
+        options: [
+            {
+                id: 21,
+                header: null,
+                itemId: null,
+                array: [
+                    {
+                        id: 211,
+                        text: "About Me",
+                        itemId: "aboutMe",
+                        path: "",
+                        active: true,
+                        isHover: "init",
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 212,
+                        text: "Our Services",
+                        itemId: "ourServices",
+                        path: "",
+                        active: false,
+                        isHover: "init",
+                        subOptions: []
+                    },
+                    {
+                        id: 213,
+                        text: "Our Process",
+                        itemId: "ourProcess",
+                        path: "",
+                        active: false,
+                        isHover: "init",
+                        subOptions: []
+                    },
+                    {
+                        id: 214,
+                        text: "FAQ Page",
+                        itemId: "faqPage",
+                        path: "",
+                        active: false,
+                        isHover: "init",
+                        subOptions: []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 3,
+        text: "Work.",
+        itemId: "work",
+        path: null,
+        active: true,
+        isHover: "init",
+        hasSubOptions: false,
+        options: [
+            {
+                id: 31,
+                header: null,
+                itemId: null,
+                array: [
+                    {
+                        id: 311,
+                        text: "Switch Image",
+                        itemId: "switchImage",
+                        path: "",
+                        active: true,
+                        isHover: "init",
+                        subOptions: []
+        
+                    },
+                    {
+                        id: 312,
+                        text: "Simple Overlay",
+                        itemId: "simpleOverlay",
+                        path: "",
+                        active: false,
+                        isHover: "init",
+                        subOptions: []
+                    },
+                    {
+                        id: 313,
+                        text: "Slide From Image Left",
+                        itemId: "slideFromImageLeft",
+                        path: "",
+                        active: false,
+                        isHover: "init",
+                        subOptions: []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 4,
+        text: "Contact.",
+        itemId: "contact",
+        path: null,
+        active: true,
+        isHover: "init",
+        hasSubOptions: false,
+        options: []
+    },
+]
