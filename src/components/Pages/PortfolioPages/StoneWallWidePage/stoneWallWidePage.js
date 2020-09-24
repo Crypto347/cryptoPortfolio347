52f48685a24d97a46d47eb30f38662e92ee42d97
Humay <qasimovahumay@gmail.com>
2020-09-24 09:09:39 +0400

    stoneWallWidePage media screen narrow 2 added

--- src/components/Pages/PortfolioPages/StoneWallWidePage/stoneWallWidePage.js
+++ src/components/Pages/PortfolioPages/StoneWallWidePage/stoneWallWidePage.js
@@ -272,12 +272,20 @@ export const StoneWallWidePage = (props) => {
             setClassNameImg6("stone-wall-wide-page-item-id6");
             setClassNameImg7("stone-wall-wide-page-item-id7");
         }
-        // if(size.width < 670 && size.width > 480 && prevScreenWidthVal > size.width){
-        //     setClassNameImg2("stone-wall-page-item-id2-animation-narrow2-screen");
-        //     setClassNameImg3("stone-wall-page-item-id3-animation-narrow2-screen");
-        //     setClassNameImg4("stone-wall-page-item-id4-animation-narrow2-screen");
-        //     setClassNameImg5("stone-wall-page-item-id5-animation-narrow2-screen");
-        // }
+        if(size.width < 670 && size.width > 480 && prevScreenWidthVal < size.width){
+            // setClassNameImg2("stone-wall-page-item-id2-animation-expand2-screen");
+            // setClassNameImg3("stone-wall-page-item-id3-animation-expand2-screen");
+            // setClassNameImg4("stone-wall-page-item-id4-animation-expand2-screen");
+            // setClassNameImg5("stone-wall-page-item-id5-animation-expand2-screen");
+        }
+        if(size.width < 670 && size.width > 480 && prevScreenWidthVal > size.width){
+            setClassNameImg2("stone-wall-wide-page-item-id2-animation-narrow2-screen");
+            setClassNameImg3("stone-wall-wide-page-item-id3-animation-narrow2-screen");
+            setClassNameImg4("stone-wall-wide-page-item-id4-animation-narrow2-screen");
+            setClassNameImg5("stone-wall-wide-page-item-id5-animation-narrow2-screen");
+            setClassNameImg6("stone-wall-wide-page-item-id6-animation-narrow2-screen");
+            setClassNameImg7("stone-wall-wide-page-item-id7-animation-narrow2-screen");
+        }
         // if(size.width < 480){
             // setClassNameImg2("stone-wall-page-item-id2");
             // setClassNameImg3("stone-wall-page-item-id3");
