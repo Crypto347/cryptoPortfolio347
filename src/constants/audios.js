4a6d50163ea61ca28f041d4d7e052ef2dc77cf6b
Humay <qasimovahumay@gmail.com>
2021-03-16 10:22:25 +0400

    Audio component updated

---
+++ src/constants/audios.js
@@ -0,0 +1,9 @@
+/**
+ * Audios
+ */
+
+import CreativeMinds from '../audio/bensound-creativeminds.mp3';
+import Ukulele from '../audio/bensound-ukulele.mp3';
+
+export const CREATIVE_MIND = CreativeMinds;
+export const UKULELE = Ukulele;
