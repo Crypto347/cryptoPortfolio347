301c0be23b161482d840bcf0a5a809cfce3494a1
Humay <qasimovahumay@gmail.com>
2021-06-04 14:22:23 +0400

    service axios comment for service updated

--- src/service/service.js
+++ src/service/service.js
@@ -1702,7 +1702,7 @@ export function fetchSubscribeContactFormPage(info, history) {
 //     })
 // })
 
-        axiosInstance(history).post( //axios.post if in index.js //axiosInstance(history).post for interceptor
+        axiosInstance(history).post( //axios.post if in index.js for interceptor //axiosInstance(history).post for axiosInstance
             `http://localhost:3005/api/subscribe-contact-form-page`,
              {
                 info
